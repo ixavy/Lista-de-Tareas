d169a09dc1646c83865b83480fb3ae598aa5469e
iXavy <ixavy@ixavy.com>
2016-06-05 00:51:35 +0200

    añadido archivo persistencia

---
+++ Lista de Tareas/Persistencia.swift
@@ -0,0 +1,13 @@
+//
+//  Persistencia.swift
+//  Lista de Tareas
+//
+//  Created by iXavy on 5/6/16.
+//  Copyright © 2016 iXavy. All rights reserved.
+//
+
+import Cocoa
+
+class Persistencia: NSObject {
+
+}
